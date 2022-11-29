8ef555dcac1a5dbd54c6e34b37c4b7521453aaeb
renpeng <renpeng@p1.com>
2022-11-29 18:25:07 +0800

    add log

--- src/test_context.go
+++ src/test_context.go
@@ -14,25 +14,43 @@ type FlashPoint struct {
 
 func main() {
 
-	old := make([]*FlashPoint, 0)
-	file, err := os.Open("/Users/lucas/Desktop/ShiningPoints.js")
+	old := make([]FlashPoint, 0)
+	file, err := os.Open("./old.json")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 	content, err := ioutil.ReadAll(file)
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	err = json.Unmarshal(content, old)
+	err = json.Unmarshal(content, &old)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Print(old)
+	new := make([]FlashPoint, 0)
+	file, err = os.Open("./new.json")
+	if err != nil {
+		panic(err)
+	}
+
+	content, err = ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = json.Unmarshal(content, &new)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(old)
+	fmt.Println(new)
 
 }
