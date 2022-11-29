8391a22cb21a51c24a8426b57a532acb562800db
renpeng <renpeng@p1.com>
2022-11-29 19:02:31 +0800

    add log

--- src/test_context.go
+++ src/test_context.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 type FlashPoint struct {
@@ -32,7 +33,7 @@ func main() {
 		return
 	}
 
-	new := make([]FlashPoint, 0)
+	new := make([]FlashPoint, 0, 2000)
 	file, err = os.Open("./new.json")
 	if err != nil {
 		panic(err)
@@ -52,5 +53,32 @@ func main() {
 
 	fmt.Println(old)
 	fmt.Println(new)
+
+	result := make(map[string]int, 0)
+	max := -100
+	for _, v := range old {
+		if _, ok := result[strings.Trim(v.LabelText, " ")]; ok {
+			continue
+		}
+		result[v.LabelText] = v.Id
+		if v.Id > max {
+			max = v.Id
+		}
+	}
+
+	i := max + 1
+	for _, v := range new {
+		if _, ok := result[strings.Trim(v.LabelText, " ")]; ok {
+			continue
+		}
+		result[v.LabelText] = i
+		i++
+	}
+
+	js, _ := json.Marshal(result)
+
+	fmt.Println("-------")
+
+	fmt.Println(string(js))
 
 }
